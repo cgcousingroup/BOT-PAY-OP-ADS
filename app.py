d39016619e7dabfa5430d85e0de22d5c763e4cc9
cgcousingroup <cgcousingroup@gmail.com>
2025-06-09 15:37:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = '7867528322:AAHrXGZ6sTJdYKZ_MCqsOlY3njaM5PFPFV0'  # Coloque seu token aqui
+TOKEN = '7867528322:AAHrXGZ6sTJdYKZ_MCqsOlY3njaM5PFPFV0'  # Seu token do BotFather
 
 # Função /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
 
-    # Envia o vídeo via URL
     video_url = 'https://firebasestorage.googleapis.com/v0/b/typebot-7660e.appspot.com/o/video_2025-06-09_15-12-24.mp4?alt=media&token=b58e3913-43e6-42ff-8f3e-2bbc0b0f604a'
-    await context.bot.send_video(chat_id=chat_id, video=video_url)
 
-    # Texto de venda
-    texto = (
-        "Só hoje: *Vitalício do Funk Clube por 5,00 + 2 grupos Bônus proibidos liberado na hora* ⚡️\n\n"
+    legenda = (
+        "*Só hoje: Vitalício do Funk Clube por 5,00 + 2 grupos Bônus proibidos liberado na hora* ⚡️\n\n"
         "🔞 *+50k de mídias nunca vista na internet*\n"
         "🍿 *Recheado de N0vinhas safadas*\n"
         "😈 *Chat exclusivo e lotado de vídeos proibidos*\n\n"
@@ -25,8 +22,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "@supvipoficial"
     )
 
-    # Envia o texto com formatação
-    await context.bot.send_message(chat_id=chat_id, text=texto, parse_mode="Markdown")
+    await context.bot.send_video(chat_id=chat_id, video=video_url, caption=legenda, parse_mode="Markdown")
 
 # Inicializa o bot
 app = ApplicationBuilder().token(TOKEN).build()
